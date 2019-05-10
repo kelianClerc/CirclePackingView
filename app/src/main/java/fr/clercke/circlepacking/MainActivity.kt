e2c5fdc3fabfa35545da291c9361e57f2cf89ad7
Kélian Clerc <kelian.clerc@fabernovel.com>
2019-05-10 23:08:55 +0200

    Dispose circle packing view before destroying activity

--- app/src/main/java/fr/clercke/circlepacking/MainActivity.kt
+++ app/src/main/java/fr/clercke/circlepacking/MainActivity.kt
@@ -2,11 +2,17 @@ package fr.clercke.circlepacking
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        circle_packing_view.dispose()
+        super.onDestroy()
     }
 }
